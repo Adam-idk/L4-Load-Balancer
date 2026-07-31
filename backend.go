8413f8619ff25895e7a8be9429982e4870a4376e
Adam <ambopk03@gmail.com>
2026-07-31 20:38:41 +0200

    implemented backend.go

--- backend.go
+++ backend.go
@@ -1,1 +1,51 @@
 package main
+
+import (
+	"sync"
+	"sync/atomic"
+)
+
+type Backend struct {
+	URL   string
+	Alive bool
+	mux   sync.RWMutex
+}
+
+func (b *Backend) SetAlive(alive bool) {
+	b.mux.Lock()
+	defer b.mux.Unlock()
+	b.Alive = alive
+}
+
+func (b *Backend) IsAlive() bool {
+	b.mux.RLock()
+	defer b.mux.RUnlock()
+	return b.Alive
+}
+
+type ServerPool struct {
+	backends []*Backend
+	current  uint64
+}
+
+func (s *ServerPool) AddBackend(b *Backend) {
+	s.backends = append(s.backends, b)
+}
+
+func (s *ServerPool) GetNextValidPeer() *Backend {
+	l := uint64(len(s.backends))
+	if l == 0 {
+		return nil
+	}
+
+	next := atomic.AddUint64(&s.current, 1)
+
+	for i := uint64(0); i < l; i++ {
+		idx := (next + i) % l
+		if s.backends[idx].IsAlive() {
+			return s.backends[idx]
+		}
+	}
+
+	return nil
+}
