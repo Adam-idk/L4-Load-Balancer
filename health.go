9b28e7cb7b15c44dcbf255c5b3c5449934c7e026
Adam <ambopk03@gmail.com>
2026-08-01 02:17:56 +0200

    implemented health.go

--- health.go
+++ health.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"net"
+	"time"
+)
+
+func CheckHealth(pool *ServerPool) {
+	for _, backend := range pool.backends {
+		conn, err := net.DialTimeout("tcp", backend.URL, 2*time.Second)
+		if err != nil {
+			backend.SetAlive(false)
+		} else {
+			backend.SetAlive(true)
+			conn.Close()
+		}
+	}
+}
+
+func StartHealthCheck(pool *ServerPool) {
+	CheckHealth(pool)
+	ticker := time.NewTicker(10 * time.Second)
+	for range ticker.C {
+		CheckHealth(pool)
+	}
+}
