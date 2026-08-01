c049503904a0f7a7ed53b99468904db0374adb24
Adam <ambopk03@gmail.com>
2026-08-01 02:45:03 +0200

    implemented main.go

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
 func main() {
+	cfg := NewConfig()
 
+	pool := NewServerPool()
+	for _, url := range cfg.Backends {
+		pool.AddBackend(NewBackend(url))
+	}
+
+	go StartHealthCheck(pool)
+
+	StartServer(cfg, pool)
 }
